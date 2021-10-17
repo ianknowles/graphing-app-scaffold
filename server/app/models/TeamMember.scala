22b41e32662ff580418c4cb126f949e67be88f6a
ianknowles <ian@imknowles.co.uk>
2021-10-17 14:08:29 +0100

    generate team member link anchor id in the model class

--- server/app/models/TeamMember.scala
+++ server/app/models/TeamMember.scala
@@ -2,4 +2,6 @@ package models
 
 import play.api.mvc.Call
 
-case class TeamMember(name: String, job: String, desc: String, imageURL: Call, links: List[Link])
+case class TeamMember(name: String, job: String, desc: String, imageURL: Call, links: List[Link]) {
+	val id: String = name.replaceAll("\\s", "")
+}
