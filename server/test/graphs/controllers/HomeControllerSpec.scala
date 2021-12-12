6d9276236c6d85205d10b42ab0811c7e1ab83159
ianknowles <ian@imknowles.co.uk>
2021-12-12 21:55:00 +0000

    fix tests for index page

--- server/test/graphs/controllers/HomeControllerSpec.scala
+++ server/test/graphs/controllers/HomeControllerSpec.scala
@@ -7,6 +7,7 @@ import org.webjars.play.WebJarsUtil
 import play.api.Configuration
 import play.api.test.Helpers._
 import play.api.test._
+import play.mvc.Http
 
 /**
  * Add your spec here.
@@ -15,35 +16,36 @@ import play.api.test._
  * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
  */
 class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+	val headers: FakeHeaders = FakeHeaders(Seq(Http.HeaderNames.HOST -> "playapp"))
 
   "HomeController GET" should {
 
     "render the index page from a new instance of controller" in {
 			implicit val webJarsUtil: WebJarsUtil = app.injector.instanceOf[WebJarsUtil]
       val controller = new HomeController(stubControllerComponents(), Configuration(ConfigFactory.load()))
-      val home = controller.index().apply(FakeRequest(GET, "/"))
+      val home = controller.index().apply(FakeRequest(GET, "/").withHeaders(headers))
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("index.jumbo.title")
     }
 
     "render the index page from the application" in {
       val controller = inject[HomeController]
-      val home = controller.index().apply(FakeRequest(GET, "/"))
+      val home = controller.index().apply(FakeRequest(GET, "/").withHeaders(headers))
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("jumbo.title")
     }
 
     "render the index page from the router" in {
-      val request = FakeRequest(GET, "/")
+      val request = FakeRequest(GET, "/").withHeaders(headers)
       val home = route(app, request).get
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      contentAsString(home) must include ("jumbo.title")
     }
   }
 }
